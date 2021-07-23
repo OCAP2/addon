dde3785803aff4fd9de3ec8cfe9eaa7ce66cac6c
Florian Kinder <florian.kinder@fankserver.com>
2021-07-23 22:18:57 +0200

    Exclude ground objects

--- userconfig/ocap/config.hpp
+++ userconfig/ocap/config.hpp
@@ -1,7 +1,7 @@
 ocap_nameServer = 'TvT';
 ocap_minPlayerCount = 60;
 ocap_frameCaptureDelay = 1;
-ocap_excludeClassFromRecord = ["ACE_friesAnchorBar"]; // excludes specific class names from recordings
+ocap_excludeClassFromRecord = ["ACE_friesAnchorBar", "GroundWeaponHolder", "WeaponHolderSimulated"]; // excludes specific class names from recordings
 ocap_excludeMarkerFromRecord = ["SystemMarker_"]; // excludes markers with any of these strings in their name
 ocap_saveMissionEnded = true;
 ocap_minMissionTime = 90;
